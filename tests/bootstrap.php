507825b4511789415046d68b9b0cd4de7d79e456
Jacques Marneweck <jacques@siberia.co.za>
2020-08-08 10:07:07 +0200

    Prep for dev

---
+++ tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+/**
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2020 Jacques Marneweck.  All rights strictly reserved.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
